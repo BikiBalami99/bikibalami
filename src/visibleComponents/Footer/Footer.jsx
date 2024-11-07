9810e4803c564d654531ee260c5a46bcf8bfc091
Biki Balami <bikibalami99@gmail.com>
2024-11-07 20:11:20 +0900

    implement footer styling

--- src/visibleComponents/Footer/Footer.jsx
+++ src/visibleComponents/Footer/Footer.jsx
@@ -1,35 +1,45 @@
 import React from "react";
+import styles from "./Footer.module.css";
+import linkedinLogo from "/assets/images/linkedinLogo.png";
+import githubLogo from "/assets/images/githubLogo.png";
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <h2>Lets Talk</h2>
+    <footer className={styles.footer}>
+      <div className={styles.left}>
         <h3>Lets make something great together.</h3>
         <p>bikibalami1999@gmail.com</p>
       </div>
 
-      <div>
-        <p>
-          I am looking to work for people that believe in changing the world
-          through digital innovation. So, please do not hesitate to reach out!
-        </p>
-        <ul>
+      <div className={styles.right}>
+        <ul className={styles.navLinks}>
           <li>Home</li>
           <li>Projects</li>
           <li>Skills</li>
           <li>My Story</li>
         </ul>
-      </div>
+        <div className={styles.social}>
+          <h3>Socials Links</h3>
+          <div className={styles.socialLinks}>
+            <div className={styles.socialsLinkIcon}>
+              <a
+                href="https://www.linkedin.com/in/biki-balami-1bb9281a3/"
+                target="_blank"
+              >
+                <img src={linkedinLogo} />
+              </a>
+            </div>
 
-      <div>
-        <h3>Socials Links</h3>
-        <p>Linkedin</p>
-        <p>Github</p>
-      </div>
-
-      <div>
-        <p>© Biki Balami 2024</p>
+            <div className={styles.socialsLinkIcon}>
+              <a href="https://github.com/BikiBalami99" target="_blank">
+                <img src={githubLogo} />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>© Biki Balami 2024</p>
+        </div>
       </div>
     </footer>
   );
