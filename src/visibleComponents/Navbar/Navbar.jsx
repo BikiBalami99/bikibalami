e1b9aa348f58b9d15a502a922a15f6b675cacd43
Biki Balami <bikibalami99@gmail.com>
2024-11-07 23:23:28 +0900

    Implement hamburger menu

--- src/visibleComponents/Navbar/Navbar.jsx
+++ src/visibleComponents/Navbar/Navbar.jsx
@@ -2,31 +2,45 @@ import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  const [showLinks, setShowLinks] = useState(false);
+  const [hideLinks, setHideLinks] = useState(true);
 
-  function toggleShowLinks() {
-    setShowLinks((prev) => !prev);
+  function togglehideLinks() {
+    setHideLinks((prev) => !prev);
   }
 
   return (
     <nav
       className={styles.navBar}
-      style={showLinks ? { height: "4rem" } : null}
+      style={hideLinks ? { height: "70px" } : null}
     >
-      <div
-        className={styles.wish}
-        style={
-          showLinks ? { opacity: "0", transform: "translateY(-50px)" } : null
-        }
-      >
+      <div className={styles.wish}>
         <p>Good Evening!</p>
       </div>
       <div className={styles.navItems}>
-        <div onClick={toggleShowLinks} className={styles.hamburger}>
-          <div className={styles.top}></div>
-          <div className={styles.middle}></div>
-          <div className={styles.bottom}></div>
+        {/* Hamburger Icon */}
+        <div onClick={togglehideLinks} className={styles.hamburger}>
+          <div
+            className={
+              hideLinks ? styles.top : `${styles.top} ${styles.topAnimation}`
+            }
+          ></div>
+          <div
+            className={
+              hideLinks
+                ? styles.middle
+                : `${styles.middle} ${styles.middleAnimation}`
+            }
+          ></div>
+          <div
+            className={
+              hideLinks
+                ? styles.bottom
+                : `${styles.bottom} ${styles.bottomAnimation}`
+            }
+          ></div>
         </div>
+
+        {/* NavLinks */}
         <ul className={styles.navLinks}>
           <li>Home</li>
           <li>Projects</li>
