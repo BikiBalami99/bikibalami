0e4c851b3b876e8373d2abd93f75a0702217f182
Biki Balami <bikibalami99@gmail.com>
2024-11-08 23:31:21 +0900

    Implement responsive project section + navBar optimization

--- src/visibleComponents/Projects/ProjectCard.jsx
+++ src/visibleComponents/Projects/ProjectCard.jsx
@@ -4,8 +4,8 @@ import styles from "./Projects.module.css";
 const ProjectCard = () => {
   return (
     <div className={styles.card}>
-      <h4>Title</h4>
-      <p>Description</p>
+      <h4>Coming Soon</h4>
+      <p>--</p>
       <div className={styles.thumbnail}></div>
     </div>
   );
--- src/visibleComponents/Projects/Projects.jsx
+++ src/visibleComponents/Projects/Projects.jsx
@@ -34,7 +34,6 @@ const Projects = () => {
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
-          <ProjectCard />
         </div>
         <div className={styles.buttons}>
           <button>&larr;</button>
